4a887d7b4a6bb840996975c4ea615e0e84cea4f4
gajonuo <gajonuco@gmail.com>
2025-02-22 17:03:31 -0300

    first commit

---
+++ java/src/test/java/com/gabriel_nunez/oficina_mecanina/java/JavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel_nunez.oficina_mecanina.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
